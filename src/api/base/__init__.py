ef7b1105f2c359a13b44e3d007d333e89f681ed6
Alex Manelis <amanelis@gmail.com>
2018-09-12 22:57:21 -0600

    Quandl base api key setting

--- src/api/base/__init__.py
+++ src/api/base/__init__.py
@@ -4,3 +4,6 @@ import quandl
 class APIBase(object):
     def __init__(self, config):
         self.config = config
+
+        # Configure needed API keys here
+        quandl.ApiConfig.api_key = os.getenv('QUANDL_API_KEY', None)
